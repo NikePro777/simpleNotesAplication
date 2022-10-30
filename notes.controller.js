296bb3b26fe0b970df0383b0760a9b25ece0ced2
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-10-30 14:08:20 +0500

    refactoring

--- notes.controller.js
+++ notes.controller.js
@@ -14,13 +14,17 @@ async function addNote(title) {
     id: Date.now().toString(),
   };
   notes.push(note);
-  await fs.writeFile(notesPath, JSON.stringify(notes));
+  await saveNotes(notes);
   console.log(chalk.bgGreen("Note was added!"));
 }
 
 async function getNotes() {
   const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
   return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes));
 }
 
 async function printNotes() {
@@ -30,21 +34,12 @@ async function printNotes() {
     console.log(chalk.red.inverse(note.id), chalk.blue(note.title));
   });
 }
+
 async function removeNote(id) {
   const notes = await getNotes();
-  let count = false;
-  let newNotes = [];
-  notes.forEach((note) => {
-    if (note.id === id) {
-      console.log(chalk.red(`note with id = ${id} was delete!`));
-      count = true;
-    } else {
-      newNotes.push(note);
-    }
-  });
-  !count
-    ? console.log(chalk.green("походу ничего не удалили("))
-    : await fs.writeFile(notesPath, JSON.stringify(newNotes));
+  const filtered = notes.filter((note) => note.id !== id);
+  await saveNotes(filtered);
+  console.log(chalk.red(`note with id = '${id}' was delete!`));
 }
 
 module.exports = {
