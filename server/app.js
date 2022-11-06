a0be46d64d96c1a7434b3a89a567c44ec1cf7cd4
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-11-06 21:07:24 +0500

    start setting server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const chalk = require("chalk");
+
+const app = express();
+
+const PORT = config.get("port") ?? 8080;
+app.listen(PORT, () =>
+  console.log(chalk.green(`server started on port ${PORT}`))
+);
